68e65e2ac444fc52125bf3b9d4022e467217f089
Adolfo20bm <abm2491@gmail.com>
2023-05-19 18:05:54 -0500

    Reinicia el identity

--- sql4.sql
+++ sql4.sql
@@ -3,3 +3,10 @@ add foreign key (idPais) references Pais(idPais)
 
 alter table HistoriaPaciente
 add foreign key (idPaciente) references Paciente(idPaciente)
+
+-- Reiniciar el identity
+-- En una base de datos cualquiera en una tabla
+-- insert into tabla1 values(1)
+-- select * from tabla1
+-- delete from tabla1
+-- dbcc checkident ('tabla1', ressed, 0)
