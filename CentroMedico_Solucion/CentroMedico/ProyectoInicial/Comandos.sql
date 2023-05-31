0902625b6c58b01ceeba23e69ad01fdcb040d9cf
Adolfo20bm <abm2491@gmail.com>
2023-05-31 11:29:40 -0500

    clausulas SQL

--- CentroMedico_Solucion/CentroMedico/ProyectoInicial/Comandos.sql
+++ CentroMedico_Solucion/CentroMedico/ProyectoInicial/Comandos.sql
@@ -4,3 +4,42 @@
 
 
 SELECT * from paciente
+
+INSERT INTO paciente (nombre, apellido, fNacimiento, domicilio, idPais, telefono, email, observacion)
+values('Diego','Perez','2013-01-04','Av Pablo Marmol 2','BRA','','','')
+
+UPDATE paciente SET observacion='pacientes creados desde UI'
+
+-- CLAUSULAS SQL
+
+-- WHERE
+
+SELECT * FROM Paciente WHERE nombre = 'claudio' and apellido='bravo'
+
+DELETE FROM Paciente where nombre = 'claudio' and apellido='bravo'
+
+UPDATE paciente SET observacion='Observacion modificada' where idPaciente=7
+
+-- TOP
+
+SELECT TOP 2 * FROM Paciente where apellido='perez'
+
+--para traer el ultimo registro
+select top 1 * from Turno order by fechaTurno desc
+
+-- ORDER BY con ASC y DESC
+
+SELECT TOP 1 * FROM Paciente ORDER BY fNacimiento ASC
+
+-- DISTINCT
+
+SELECT DISTINCT (apellido) from Paciente
+
+SELECT DISTINCT idpaciente, apellido from Paciente
+
+-- GROUP BY
+SELECT * FROM Paciente GROUP BY apellido
+SELECT apellido FROM Paciente GROUP BY apellido
+
+
+
